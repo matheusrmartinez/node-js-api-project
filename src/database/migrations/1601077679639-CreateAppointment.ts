69d895d3783c2858ef8fc0eea7ba0f1359d9db21
Matheus Martinez <matheus.rmartinez@gmail.com>
2020-09-26 20:31:14 -0300

    Criação de repositório de Agendamento - Criação de rota de agendamento - Criação do serviço de agendamento

--- src/database/migrations/1601077679639-CreateAppointment.ts
+++ src/database/migrations/1601077679639-CreateAppointment.ts
@@ -12,6 +12,7 @@ export default class CreateAppointment1601077679639
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
