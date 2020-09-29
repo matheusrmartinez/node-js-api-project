2b0c214ce207a2732aa43f5a8cf178c4d2f84894
Matheus Martinez <matheus.rmartinez@gmail.com>
2020-09-29 20:03:13 -0300

    Criação de global error handling

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
 
 interface TokenPayLoad {
   iat: string;
@@ -16,21 +17,19 @@ export default function ensureAuthenticated(
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new Error('JWT token is missing');
+    throw new AppError('JWT token is missing', 401);
   }
 
   const [, token] = authHeader.split(' ');
 
-  try {
-    const decoded = verify(token, authConfig.jwt.secret);
+  const decoded = verify(token, authConfig.jwt.secret);
 
-    const { sub } = decoded as TokenPayLoad;
+  const { sub } = decoded as TokenPayLoad;
 
-    request.user = {
-      id: sub,
-    };
-    return next();
-  } catch {
-    throw new Error('Invalid JTW token');
-  }
+  request.user = {
+    id: sub,
+  };
+  return next();
+
+  throw new AppError('Invalid JTW token', 401);
 }
--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -4,21 +4,17 @@ import AuthenticationuUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticationUser = new AuthenticationuUserService();
-    const { user, token } = await authenticationUser.execute({
-      email,
-      password,
-    });
+  const authenticationUser = new AuthenticationuUserService();
+  const { user, token } = await authenticationUser.execute({
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json({ user, token });
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json({ user, token });
 });
 
 export default sessionsRouter;
